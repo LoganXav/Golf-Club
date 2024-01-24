6f503831fbb8511757dc28dfdb9897accaef211e
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-01-24 12:10:13 +0100

    feat(module): Add commit messages linter

---
+++ commitlint.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'perf',
+        'revert',
+        'custom', // Custom commit type
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['project', 'module', 'component', 'other', ''],
+    ],
+    'subject-case': [1, 'always', 'sentence-case'], // Warn if subject case is incorrect
+  },
+};
