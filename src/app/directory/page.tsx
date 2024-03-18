56a7735bce69d5dac6055a84f249f5d8cdc4f895
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-03-18 16:44:29 +0100

    fix(module): deployment bug fixes

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,10 +1,8 @@
 import { MembersList } from '@/components/lists';
 import { getMembersAction } from '../_actions/member';
 
-async function Directory() {
+export default async function Directory() {
   const response = await getMembersAction();
 
   return <MembersList response={response} />;
 }
-
-export default Directory;
