6d16e5a9a2f5048247a562e3995bba19ec2bf9a7
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-03-25 13:45:41 +0100

    refactor(module): payment option radio component

--- src/components/forms/steps/premium-payment-info.tsx
+++ src/components/forms/steps/premium-payment-info.tsx
@@ -3,6 +3,7 @@ import { Label } from '@/components/ui/label';
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 import * as React from 'react';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio';
 
 interface Props {
   setPaymentMethod: React.Dispatch<React.SetStateAction<string>>;
@@ -12,23 +13,32 @@ export default function PremiumPaymentInfo({ setPaymentMethod }: Props) {
   return (
     <div className="space-y-4">
       <Label>Select a payment option</Label>
-      <div className="grid gap-4 lg:grid-cols-2">
+      <RadioGroup className="grid gap-4 lg:grid-cols-2" defaultValue="paystack">
         <Button
           type="button"
           onClick={() => setPaymentMethod('paystack')}
-          className="flex min-w-[10rem] items-center justify-center rounded-md border bg-input py-8 shadow-none focus:border-primary"
+          className="flex min-w-[10rem] items-center justify-center gap-4 overflow-hidden rounded-md border bg-input py-8 shadow-none focus:border-primary"
         >
-          <Icons.paystack className="w-36 lg:w-44" />
+          <RadioGroupItem className="h-6 w-6" value="paystack" id="paystack" />
+          <Label className="cursor-pointer" htmlFor="paystack">
+            <Icons.paystack className="w-36 lg:w-44" />
+          </Label>
         </Button>
-
         <Button
           type="button"
           onClick={() => setPaymentMethod('flutterwave')}
-          className="flex min-w-[10rem] items-center justify-center rounded-md border bg-input py-8 shadow-none focus:border-primary"
+          className="flex min-w-[10rem] items-center justify-center gap-4 overflow-hidden rounded-md border bg-input py-8 shadow-none focus:border-primary"
         >
-          <Icons.flutterwave className="w-36 lg:w-44" />
+          <RadioGroupItem
+            className="h-6 w-6"
+            value="flutterwave"
+            id="flutterwave"
+          />
+          <Label className="cursor-pointer" htmlFor="flutterwave">
+            <Icons.flutterwave className="w-36 lg:w-44" />
+          </Label>
         </Button>
-      </div>
+      </RadioGroup>
     </div>
   );
 }
