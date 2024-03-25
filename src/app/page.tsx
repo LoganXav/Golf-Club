a252f3d20934bb89e8903f1404bd51d8ca1cb531
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-03-25 11:08:05 +0100

    fix(module): paystack redirect url fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function IndexPage() {
   const options = [
     {
       title: 'Premium',
-      price: 'N20 000',
+      price: '$20',
       header: 'Exclusive golf experience',
       content:
         'Unlock premium features and privileges with our premium membership.',
@@ -15,7 +15,7 @@ export default function IndexPage() {
     },
     {
       title: 'Standard',
-      price: 'N0',
+      price: '$0',
       header: 'Explore the world of golf',
       content:
         'For this package, you can register your details and get standard golf.',
