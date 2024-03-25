a5cfd9c9ac43779b8fdf0ab2ff24e4fc380c5c91
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-03-25 11:20:50 +0100

    fix(module): paystack redirect url fix

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,6 +1,8 @@
 import { MembersList } from '@/components/lists';
 import { getMembersAction } from '../_actions/member';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Directory() {
   const response = await getMembersAction();
 
