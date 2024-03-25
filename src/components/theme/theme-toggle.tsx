38159695a1e42cb7f2cb89f2bfb3225d4db158f0
Timilehin Sogbesan <sogbesansegun22@gmail.com>
2024-03-25 11:39:38 +0100

    fix(module): theme toggle dark mode fix

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import * as React from 'react';
 import { useTheme } from 'next-themes';
 import { ThemeToggler } from './theme-toggler';
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+
+  const initialTheme = prefersDarkMode ? 'dark' : 'light';
+
+  React.useEffect(() => {
+    if (!theme) {
+      setTheme(initialTheme);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialTheme]);
 
   return (
     <div>
