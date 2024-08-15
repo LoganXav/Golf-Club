a6f0c33b57abcb3c70679bc38eaac09a07320037
Segun <sogbesansegun22@gmail.com>
2024-08-15 19:46:40 +0100

    Chore: add no database found error message

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,9 +1,21 @@
 import { MembersList } from '@/components/lists';
 import { getMembersAction } from '../_actions/member';
+import { env } from '@/env.mjs';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Directory() {
+  if (!env.DATABASE_URL) {
+    return (
+      <MembersList
+        response={{
+          data: [],
+          type: 'Error',
+          message: 'Could not find database.',
+        }}
+      />
+    );
+  }
   const response = await getMembersAction();
 
   return <MembersList response={response} />;
